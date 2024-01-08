9061e70459f74d089438eb04b5f472f4b59c31a5
crescentlymon <2376355613@qq.com>
2024-01-08 21:17:19 +0800

    项目初始化

---
+++ src/main/java/com/crescentltmon/bigeventbackend/BigeventBackendApplication.java
@@ -0,0 +1,13 @@
+package com.crescentltmon.bigeventbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigeventBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BigeventBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/crescentltmon/bigeventbackend/BigeventBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crescentltmon.bigeventbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigeventBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
